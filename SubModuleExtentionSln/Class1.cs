902f98cf9c29a0c067197f50d444f14b14fd4f93
Noel Adrien <adrien.noel01@gmail.com>
2022-03-16 11:27:46 +0100

    comit

---
+++ SubModuleExtentionSln/Class1.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace SubModuleExtentionSln
+{
+  public class Customer
+  {
+    public int Tune { get; set; }
+    public int GetTune()
+    {
+      return this.Tune + 100;
+    }
+  }
+}
